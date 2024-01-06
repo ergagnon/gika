fcb8925a96cf758c21362225c806f36033ac4d77
Eric Gagnon <ergagnon@gmail.com>
2024-01-06 16:13:23 -0500

    feat: grpc compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     kotlin("jvm") version "1.9.20"
+    id("com.google.protobuf") version "0.9.4"
     application
 }
 
@@ -15,14 +18,45 @@ dependencies {
     implementation("org.apache.tika:tika-core:2.8.0")
     implementation("org.apache.tika:tika-parsers-standard-package:2.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
+    implementation("io.grpc:grpc-protobuf:1.59.1")
+    implementation("io.grpc:grpc-protobuf-lite:1.59.1")
+    implementation("io.grpc:grpc-stub:1.59.1")
+    implementation("io.grpc:grpc-netty:1.59.1")
+    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+}
+
+kotlin {
+    jvmToolchain(19)
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.22.2"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.59.1"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(8)
 }
 
 application {
